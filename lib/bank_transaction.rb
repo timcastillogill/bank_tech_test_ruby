3dce612d035a4b38f49ee27e71b46a80d005a27e
Tim Castillo-Gill <timgillviola@gmail.com>
2020-11-30 17:30:10 +0000

    Refactored the hash to have a key with array of amount and date

--- lib/bank_transaction.rb
+++ lib/bank_transaction.rb
@@ -3,19 +3,29 @@ class BankTransaction
   attr_reader :credit, :debit
 
   def initialize
-    @credit = Hash.new { | amount, date | amount[date] }
-    @debit = Hash.new { | amount, date | amount[date] }
+    @credit = Hash.new { | amount, date | amount[date] = [] }
+    @debit = Hash.new { | amount, date | amount[date] = [] }
   end
 
   def deposit(money)
-    @credit[money] = date
+    counter = 0
+    @credit[counter += 1] = [money, date]
   end
   
   def withdrawl(money)
-    @debit[money] = date
+    counter = 0
+    @debit[counter += 1] = [money, date]
+  end
+
+  def total_credit
+    @credit.each { | amount, date | 
+      amount += amount
+    }
   end
   
-  
+  # def balance
+  #   total_credit = @credit.each
+  # end
   
   def date
     current = Date.today
--- spec/bank_transaction_spec.rb
+++ spec/bank_transaction_spec.rb
@@ -11,21 +11,35 @@ describe BankTransaction do
 
   it 'accepts a deposit amount of £100.00' do
     banking.deposit(100)
-    expect(banking.credit).to include(100.00)
+    expect(banking.credit).to include {100}
   end
 
   it 'accepts a withdrawl amount of £100.00' do
     banking.withdrawl(100)
-    expect(banking.debit).to include(100.00)
+    expect(banking.debit).to include{100.00}
   end
 
   it 'adds the date when money is deposited' do
     banking.deposit(50)
-    expect(banking.credit).to include 50 => date
+    expect(banking.credit).to include {[50, date]}
   end
 
   it 'adds the date when money is withdrawn' do
     banking.withdrawl(50)
-    expect(banking.debit).to include 50 => date
+    expect(banking.debit).to include {[50, date]}
   end
+
+  # it 'will calculate the total credit in a clients account' do
+  #   banking.deposit(200)
+  #   banking.deposit(400)
+  #   expect(banking.total_credit).to eq 600
+  # end
+
+
+  # it 'will calculate the clients balance' do
+  #   banking.deposit(1000)
+  #   banking.withdrawl(100)
+  #   expect(banking.balance).to eq 900.00
+  # end
+
 end
